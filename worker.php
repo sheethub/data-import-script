2149456f19f273b9edf3f9f51b7e8b01dedf8949
Ronny Wang <ronnywang@gmail.com>
2015-06-05 16:47:39 +0800

    加上 worker

---
+++ worker.php
@@ -0,0 +1,21 @@
+<?php
+
+$jobs = array(
+    '5284.taipei.gov.tw' => array(
+        'script' => '5284.taipei.gov.tw/update.php',
+    ),
+);
+
+putenv('SCRIPT_TIMEOUT=120');
+
+while (true) {
+    $now = microtime(true);
+    foreach ($jobs as $id => $config) {
+        if (array_key_exists('prev_time', $config) and $now - $config['prev_time'] < 60) {
+            continue;
+        }
+        $config['prev_time'] = $now;
+
+        system("php " . $config['script']);
+    }
+}
