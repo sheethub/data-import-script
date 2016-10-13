fcc47f88bfb6c6bbfd3bdb4f9146e295c13c6994
Ronny Wang <ronnywang@gmail.com>
2016-10-13 17:13:13 +0800

    改用 data.govapi.tw 取代程式，這樣只需要維護一邊就好

--- data.gov.tw/DataGovTw.php
+++ data.gov.tw/DataGovTw.php
@@ -17,54 +17,8 @@ class DataGovTw
      */
     public function getMetaFromPortal($url)
     {
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $content = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        if ($info['http_code'] != 200) {
-            throw new Exception("找不到這資料, $url (code={$info['http_code']})");
-        }
-        curl_close($curl);
-
-        $doc = new DOMDocument;
-        @$doc->loadHTML($content);
-
-
-        $ret = array();
-        $ret['title'] = trim($doc->getElementsByTagName('h1')->item(0)->nodeValue);
-        foreach ($doc->getElementsByTagName('th') as $th_dom) {
-            $name = $th_dom->nodeValue;
-            $value_dom = $th_dom->nextSibling;
-            while ($value_dom) {
-                if ($value_dom->nodeType == XML_ELEMENT_NODE) {
-                    break;
-                }
-                $value_dom = $value_dom->nextSibling;
-            }
-            if (!$value_dom) {
-                continue;
-            }
-
-            if ($name == '資料資源') {
-                $ret[$name] = array();
-                foreach ($value_dom->getElementsByTagName('a') as $a_dom) {
-                    if (in_array(strtolower($a_dom->nodeValue), array('doc', 'word', 'pdf', 'webservice'))) {
-                        continue;
-                    }
-                    if ($a_dom->nodeValue == '檢視資料' or trim($a_dom->nodeValue) == '') {
-                        continue;
-                    }
-                    $ret[$name][] = array(
-                        'type' => strtolower($a_dom->nodeValue),
-                        'url' => 'http://data.gov.tw' . $a_dom->getAttribute('href'),
-                    );
-                }
-            } elseif ($name == '資料集評分') {
-                continue;
-            } else {
-                $ret[$name] = trim($value_dom->nodeValue);
-            }
-        }
+        $url = str_replace('data.gov.tw', 'data.govapi.tw', $url);
+        $ret = json_decode(file_get_contents($url), true)['data'];
         return $ret;
     }
 
